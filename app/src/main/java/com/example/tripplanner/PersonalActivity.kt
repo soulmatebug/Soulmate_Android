11eb506bed14138f42f5fab05469ebad505183c8
b1ctory <suebin710@gmail.com>
2019-05-13 19:43:18 +0900

    personalActivity수정

--- app/src/main/java/com/example/tripplanner/PersonalActivity.kt
+++ app/src/main/java/com/example/tripplanner/PersonalActivity.kt
@@ -3,9 +3,7 @@ package com.example.tripplanner
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import com.example.tripplanner.Data.UserData
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -20,18 +18,18 @@ class PersonalActivity : AppCompatActivity() {
         var submitBtn : Button = this.findViewById(R.id.submit)
         val email = findViewById<EditText>(R.id.e_mail_input).text.toString()
         val name = findViewById<EditText>(R.id.name_input).text.toString()
+        //val birthday = findViewById<DatePicker>(R.id.birthday)
+        val sex = findViewById<RadioGroup>(R.id.sex)
         val pw = findViewById<EditText>(R.id.password_input).text.toString()
         val pw_check = findViewById<EditText>(R.id.password_test_input).text.toString()
         database = FirebaseDatabase.getInstance().reference
 
         submitBtn.setOnClickListener{
-            if(email.length == 0 || pw.length == 0 ||
-                    pw_check.length == 0){
+            if(email.length == 0 || pw.length == 0 || pw_check.length == 0){
                 Toast.makeText(this,"입력되지 않은 칸이 있습니다.",Toast.LENGTH_SHORT).show()
             }
             else{
                 writeNewUser(email,name,pw)
-                //요기서 입력값들 가져다가 저장
             }
         }
 
