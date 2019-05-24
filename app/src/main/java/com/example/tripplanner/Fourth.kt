8c22041fd9b2b0c969177ab235eb4ca78b01b470
b1ctory <suebin710@gmail.com>
2019-05-24 20:13:12 +0900

    tablayout / viewpager 추가

---
+++ app/src/main/java/com/example/tripplanner/Fourth.kt
@@ -0,0 +1,18 @@
+package com.example.tripplanner
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.tripplanner.R
+
+class Fourth : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_fourth, container, false)
+    }
+}
--- app/src/main/java/com/example/tripplanner/LoginActivity.kt
+++ app/src/main/java/com/example/tripplanner/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.tripplanner
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import org.jetbrains.anko.startActivity
@@ -15,6 +16,12 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+       val fragmentAdaper = PageAdapter(supportFragmentManager)
+        viewpager_main.adapter = fragmentAdaper
+
+        tabs.setupWithViewPager(viewpager_main)
+
         auth = FirebaseAuth.getInstance()
         btn_logout.setOnClickListener {
             auth.signOut()
